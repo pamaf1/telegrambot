e2c41ea8d3d75b86a6ba8fd8dd66b33c1454d1bc
pamaf1 <61753383+pamaf1@users.noreply.github.com>
2020-05-26 15:29:27 +0300

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/MusicResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+    class MusicResponse
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
---
+++ ConsoleApp22/ConsoleApp22/NewsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+    class NewsResponse
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+
+    }
+}
