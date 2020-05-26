58424f15b2752dfbf1cece5b8d02b33ae12c9266
pamaf1 <61753383+pamaf1@users.noreply.github.com>
2020-05-26 15:33:51 +0300

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/TemperatureInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp22
+{
+    class TemperatureInfo
+    {
+        public float temp { get; set; }
+        public float feels_Like { get; set; }       
+        public string description { get; set; }
+    }
+}
